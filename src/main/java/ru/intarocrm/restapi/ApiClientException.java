911d57e7cd8a57af5d9ac9ebc3b15c61329af742
Dmitry Mamontov <d.slonyara@gmail.com>
2014-10-13 14:09:42 +0400

    initial commit

---
+++ src/main/java/ru/intarocrm/restapi/ApiClientException.java
@@ -0,0 +1,15 @@
+package ru.intarocrm.restapi;
+
+public class ApiClientException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+
+    public ApiClientException(String text) {
+        super(text);
+    }
+
+    public ApiClientException(String text, Exception innerEx) {
+        super(text, innerEx);
+    }
+
+}
